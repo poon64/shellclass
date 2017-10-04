08e50181848b7d7681f83b6a1590c04006079224
Peter O Nilsson <peter.o.nilsson@gmail.com>
2017-10-04 22:15:26 +0200

    Added luser-demo03

---
+++ localusers/luser-demo03.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+# Display the UID and username of the user executing this script. 
+# Display if the user is the vagrant user or not. 
+
+# Disply the UID
+echo "You UID is ${UID}"
+
+# Only display if the UID does NOT match 1000.
+UID_TO_TEST_FOR='1000'
+if [[ "${UID}" -ne "${UID_TO_TEST_FOR}" ]]
+then 
+    echo "Your UID does not match ${UID_TO_TEST_FOR}."
+    exit 1
+fi
+
+# Display the username. 
+USER_NAME=$(id -un)
+
+# Test if the command succeded. 
+if [[ "${?}" -ne 0 ]]        # The ? holds the result of the last executed command, here id -un
+then 
+    echo 'The id command did not execute succesfully.'
+    exit 1
+fi
+echo "Your username is ${USER_NAME}"
+
+# You can use a string test conditional. 
+USER_NAME_TO_TEST_FOR='peter' 
+if [[ "${USER_NAME}" = "${USER_NAME_TO_TEST_FOR}" ]]
+then
+    echo "You username matches ${USER_NAME_TO_TEST_FOR}."
+fi
+
+# Test for != (not equal) for the string.
+if [[ "${USER_NAME}" != "${USER_NAME_TO_TEST_FOR}" ]]
+then
+    echo "Your username does not match ${USER_NAME_TO_TEST_FOR}."
+    exit 1
+fi
+
+exit 0
